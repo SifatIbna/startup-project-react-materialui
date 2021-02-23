d521ba37d18ce412a1362288b82a095dbf5a9e49
Sifat Ibna Amin <1605112@ugrad.cse.buet.ac.bd>
2021-02-23 22:41:23 +0600

    finished designing header for the app

---
+++ src/components/ui/Header.js
@@ -0,0 +1,77 @@
+import React from 'react';
+import AppBar from '@material-ui/core/AppBar';
+import Toolbar from '@material-ui/core/Toolbar';
+import useScrollTrigger from '@material-ui/core/useScrollTrigger';
+import {makeStyles} from "@material-ui/styles";
+import {Tabs,Tab} from '@material-ui/core';
+import  Button from '@material-ui/core/Button';
+
+import logo from '../../assets/learning_app.png';
+
+
+function ElevationScroll(props) {
+    const { children } = props;
+
+    const trigger = useScrollTrigger({
+        disableHysteresis: true,
+        threshold: 0,
+    });
+
+    return React.cloneElement(children, {
+        elevation: trigger ? 4 : 0,
+    });
+}
+
+const useStyles = makeStyles(theme => ({
+    toolbarMargin : {
+        ...theme.mixins.toolbar,
+        marginBottom: "3em"
+    },
+    logo : {
+        height: '7em',
+    },
+    tabsContainer : {
+        marginLeft: "auto",
+    },
+    tab : {
+        ...theme.typography.tab,
+        minWidth :10,
+        marginLeft: '25px',
+    },
+    button : {
+        ...theme.typography.estimate,
+        borderRadius :'50px',
+        marginLeft :'50px',
+        marginRight : '25px',
+        height : '45px',
+
+    }
+}));
+
+
+export  default function Header(props) {
+
+    const classes = useStyles();
+
+    return (
+        <React.Fragment>
+            <ElevationScroll>
+                <AppBar position={"fixed"}>
+                    <Toolbar disableGutters>
+                        <img alt={'company logo'} className={classes.logo} src={logo} />
+                        <Tabs className={classes.tabsContainer}>
+                            <Tab className={classes.tab} label={'Home'} />
+                            <Tab className={classes.tab} label={'Services'} />
+                            <Tab className={classes.tab} label={'The Revolution'} />
+                            <Tab className={classes.tab} label={'About Us'} />
+                            <Tab className={classes.tab} label={'Contact Us'} />
+                        </Tabs>
+                        <Button variant={"contained"} color={"secondary"}
+                        className={classes.button}>Free Estimate</Button>
+                    </Toolbar>
+                </AppBar>
+            </ElevationScroll>
+            <div className={classes.toolbarMargin}/>
+        </React.Fragment>
+    );
+}
